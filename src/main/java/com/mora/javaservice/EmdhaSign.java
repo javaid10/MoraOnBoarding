5972aef5fd995dda772c2fd91839778ca20ea339
Rajath <rvgofmys@gmail.com>
2022-11-28 14:43:43 +0400

    emdha changes

--- src/main/java/com/mora/javaservice/EmdhaSign.java
+++ src/main/java/com/mora/javaservice/EmdhaSign.java
@@ -1,7 +1,16 @@
 package com.mora.javaservice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import com.dbp.core.error.DBPApplicationException;
+import com.dbp.core.fabric.extn.DBPServiceExecutorBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.konylabs.middleware.common.JavaService2;
 import com.konylabs.middleware.controller.DataControllerRequest;
 import com.konylabs.middleware.controller.DataControllerResponse;
@@ -11,38 +20,156 @@ import com.mora.util.ErrorCodeMora;
 
 public class EmdhaSign implements JavaService2 {
 
-	private static final Logger logger = LogManager.getLogger(EmdhaSign.class);
+    private static final Logger logger = LogManager.getLogger(EmdhaSign.class);
 
-	@Override
-	public Object invoke(String methodId, Object[] inputArray, DataControllerRequest dcRequest,
-			DataControllerResponse dcResponse) throws Exception {
-		Result result = new Result();
+    @Override
+    public Object invoke(String methodId, Object[] inputArray, DataControllerRequest dcRequest,
+            DataControllerResponse dcResponse) throws Exception {
+        Result result = new Result();
 
-		if (preProcess(dcRequest, dcResponse, result)) {
-			if (dcRequest.getParameter("documentType").equals("1")) {
+        if (preProcess(dcRequest, dcResponse, result)) {
+            if (dcRequest.getParameter("documentType").equals("1")) {
+
+                String natid = dcRequest.getParameter("nationalId");
 //				result.addParam(new Param("status", "sucess"));
 //				result.addParam(new Param("documentHash","8f13b6b53259601353e8e84851fc069defb5730429cffe2c0fb2b7df6ca9001f"));
 //				result.addParam(new Param("transactionId","8cf11f09066647f1a7160e43b1e3c7ef"));
-			} else if (dcRequest.getParameter("documentType").equals("2")) {
+
+//			    dbxdb_sp_get_details_for_emdha
+//			    nationalId
+//			    dbxdb_sp_update_portal_emdha_signing
+//			    applctn_id
+//			    signed_doc
+                String getDetails = getCustomDetails(natid);
+                JSONObject customDetails = new JSONObject(getDetails);
+
+                if (customDetails.getJSONArray("records").getJSONObject(0).length() > 0) {
+                    HashMap<String, Object> params = new HashMap<>();
+                    params.put("signedBy",
+                            customDetails.getJSONArray("records").getJSONObject(0).optString("FullName"));
+                    params.put("arabicName",
+                            customDetails.getJSONArray("records").getJSONObject(0).optString("ArFullName"));
+                    params.put("mobile", customDetails.getJSONArray("records").getJSONObject(0).optString("mobile"));
+                    params.put("kycId", natid);
+                    params.put("email", customDetails.getJSONArray("records").getJSONObject(0).optString("Value"));
+                    params.put("address",
+                            customDetails.getJSONArray("records").getJSONObject(0).optString("addressLine1"));
+                    params.put("regionProvince",
+                            customDetails.getJSONArray("records").getJSONObject(0).optString("City_id"));
+                    params.put("docBase64",
+                            customDetails.getJSONArray("records").getJSONObject(0).optString("loan_contract"));
+                    params.put("kycId", natid);
+                    String res = DBPServiceExecutorBuilder.builder().withServiceId("MSDocumentMora")
+                            .withOperationId("EmdhaSign").withRequestParameters(params).build()
+                            .getResponse();
+                    JSONObject jsonRes = new JSONObject(res);
+
+                    if (updateIntoAuditLog(natid, dcRequest, params, res, "MSDocumentMora : EmdhaSign")) {
+                        if (updateDBAfterSign(
+                                jsonRes.optJSONArray("returnValues").getJSONObject(0).optString("signedDocument"),
+                                customDetails.getJSONArray("records").getJSONObject(0).optString("currentAppId"))) {
+                            result.addParam(new Param("status", "sucess"));
+                            result.addParam(new Param("documentHash",
+                                    jsonRes.optJSONArray("returnValues").getJSONObject(0).optString("documentHash")));
+                            result.addParam(new Param("transactionId",
+                                    jsonRes.optJSONArray("returnValues").getJSONObject(0).optString("transactionId")));
+                        }
+                    }
+
+                } else {
+
+                    result = ErrorCodeMora.ERR_100131.updateResultObject(result);
+
+                }
+
+            } else if (dcRequest.getParameter("documentType").equals("2")) {
+
 //				result.addParam(new Param("status", "sucess"));
 //				result.addParam(new Param("documentHash","8f13b6b53259601353e8e84851fc069defb5730429cffe2c0fb2b7df6ca9001f"));
 //				result.addParam(new Param("transactionId","8cf11f09066647f1a7160e43b1e3c7ef"));
-			}
-		}
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private boolean preProcess(DataControllerRequest dcRequest, DataControllerResponse dcResponse, Result result) {
-		if (dcRequest.getParameter("nationalId").isEmpty()) {
-			ErrorCodeMora.ERR_100116.updateResultObject(result);
-			return false;
-		} else if (dcRequest.getParameter("documentType").isEmpty()) {
-			ErrorCodeMora.ERR_100117.updateResultObject(result);
-			return false;
-		} else {
-			return true;
-		}
+    private boolean preProcess(DataControllerRequest dcRequest, DataControllerResponse dcResponse, Result result) {
+        if (dcRequest.getParameter("nationalId").isEmpty()) {
+            ErrorCodeMora.ERR_100116.updateResultObject(result);
+            return false;
+        } else if (dcRequest.getParameter("documentType").isEmpty()) {
+            ErrorCodeMora.ERR_100117.updateResultObject(result);
+            return false;
+        } else {
+            return true;
+        }
 
-	}
+    }
+
+    private String getCustomDetails(String natId) {
+        String res = "";
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("nationalId", natId);
+        try {
+            res = DBPServiceExecutorBuilder.builder().withServiceId("DBMoraServices")
+                    .withOperationId("dbxdb_sp_get_details_for_emdha").withRequestParameters(params).build()
+                    .getResponse();
+        } catch (DBPApplicationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    private boolean updateDBAfterSign(String signedDoc, String applicationId) {
+        boolean check = false;
+        try {
+            signedDoc = signedDoc.replaceAll("\n", "");
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("applctn_id", applicationId);
+            params.put("signed_doc", signedDoc);
+            String res = DBPServiceExecutorBuilder.builder().withServiceId("DBMoraServices")
+                    .withOperationId("dbxdb_sp_update_portal_emdha_signing_cus").withRequestParameters(params).build()
+                    .getResponse();
+            JSONObject jsonRes = new JSONObject(res);
+            if (jsonRes.optInt("opstatus") == 0)
+                check = true;
+            logger.error("updateDBAfterSign check= " + check);
+        } catch (DBPApplicationException e) {
+            logger.error("updateDBAfterSign = " + e.getMessage());
+            e.printStackTrace();
+        }
+        return check;
+    }
+
+    private boolean updateIntoAuditLog(String nationalId, DataControllerRequest request, HashMap<String, Object> params,
+            String res, String apiURL) {
+        boolean check = false;
+        try {
+            UUID uuid = UUID.randomUUID();
+            String uuidAsString = uuid.toString();
+            String cusId = nationalId;
+            String channelDevice = "Spotlight Portal";
+            String ipAddress = request.getRemoteAddr();
+            HashMap<String, Object> logdataRequestMap = new HashMap<>();
+            logdataRequestMap.put("id", uuidAsString);
+            logdataRequestMap.put("Customer_id", cusId);
+            logdataRequestMap.put("Application_id", "");
+            logdataRequestMap.put("channelDevice", channelDevice);
+            logdataRequestMap.put("apihost", apiURL);
+            logdataRequestMap.put("request_payload", (new ObjectMapper()).writeValueAsString(params));
+            logdataRequestMap.put("reponse_payload", res);
+            logdataRequestMap.put("ipAddress", ipAddress);
+            String response = DBPServiceExecutorBuilder.builder().withServiceId("DBMoraServices")
+                    .withOperationId("dbxlogs_auditlog_create").withRequestParameters(logdataRequestMap).build()
+                    .getResponse();
+            if (response != null && response.length() > 0)
+                check = true;
+            logger.error("updateIntoAuditLog check = " + check);
+        } catch (DBPApplicationException | com.fasterxml.jackson.core.JsonProcessingException e) {
+            logger.error("updateIntoAuditLog = " + e.getMessage());
+            e.printStackTrace();
+        }
+        return check;
+    }
 }
